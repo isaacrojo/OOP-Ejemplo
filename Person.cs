ccea260eb79508e3e2eab2c8815f86e99b1e46c8
Isaac Rojo <you@example.com>
2020-02-19 11:32:22 -0700

    Ejemplo practico de Programacion Orientada a Objetos

---
+++ Person.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace persons
+
+{
+class Person {
+    private string name;       //atributo
+    
+    static public string bloodColor = "red";
+
+    public Person(string name) {           //Esto es el metodo contructor : modificador de acceso- public , private / 
+
+        this.name = name;
+
+    }
+
+    public void SayHello (){
+        Console.WriteLine("Hello, my name is " + this.name );
+    }
+
+    static public void PrintRace () {
+        Console.WriteLine("Human");
+
+    }
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace persons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jack = new Person("Jack Sparrow");
+            jack.SayHello();
+
+            Person jimmy = new Person("Jimmy Neutron");
+            jimmy.SayHello();
+
+            Console.WriteLine(Person.bloodColor);            //se manda llamar desde clase Person
+            Person.PrintRace();
+            
+
+        }
+    }
+}
